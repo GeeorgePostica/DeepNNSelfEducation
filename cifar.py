a47691e12de81db4be5e0bd12fb0889a1592eda0
George Postica <ggpostica@gmail.com>
2017-05-18 00:04:01 +0200

    Almost completed load_CIFAR function...

--- cifar.py
+++ cifar.py
@@ -1,16 +1,23 @@
 from pickle import load
 from os import listdir
-from os.path import join, isfile
+from os.path import join, isfile, splitext
 
-def read_CIFAR10(cifar_folder, nr_training, nr_test, nr_validation=0):
-    filenames = [f for f in listdir(cifar_folder if isfile(f))
-    data = dict
+def read_CIFAR10(cifar_folder):
+    filenames = [f for f in listdir(cifar_folder) if (splitext(f)[1] != '.html' and splitext(f)[1] != '.meta')]
+    Xtr = Ytr = Xte = Yte = []
     for filename in filenames:
-        print(filename)
         with open(join(cifar_folder, filename), 'rb') as fo:
-            new_data = load(fo, encoding='bytes')
-            print(new_data)
-            data += load(fo, encoding='bytes')
+            data = load(fo, encoding='bytes')
+            print(filename)
+            print(data)
+            if data['batch_label'].startswith('training'):
+                Xtr.append(data['data'])
+                Ytr.append(data['labels'])
+            else:
+                Xte.append(data['data'])
+                Yte.append(data['labels'])
 
-    print(data)
-    return data
+    return Xtr, Ytr, Xte, Xte
+
+if __name__ == '__main__':
+    read_CIFAR10('cifar-10-batches-py')
