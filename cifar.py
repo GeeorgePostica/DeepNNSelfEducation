90ed7bef61e41d296fea69e44aeed6edbc25c38b
George Postica <ggpostica@gmail.com>
2017-05-18 12:24:31 +0200

    First beta version of the function

--- cifar.py
+++ cifar.py
@@ -2,7 +2,7 @@ from pickle import load
 from os import listdir
 from os.path import join, isfile, splitext
 
-def read_CIFAR10(cifar_folder):
+def read_CIFAR10(cifar_folder, train_ratio=0):
     filenames = [f for f in listdir(cifar_folder) if (splitext(f)[1] != '.html' and splitext(f)[1] != '.meta')]
     Xtr = Ytr = Xte = Yte = []
     for filename in filenames:
@@ -16,8 +16,16 @@ def read_CIFAR10(cifar_folder):
             else:
                 Xte.append(data['data'])
                 Yte.append(data['labels'])
-
-    return Xtr, Ytr, Xte, Xte
+    if 0 < train_ratio < 1:
+        X = Xtr + Xte
+        Y = Ytr + Yte
+        nr_train = int(len(X) * train_ratio)
+        # nr_test = len(X) - nr_train
+        Xtr = X[:nr_train]
+        Ytr = Y[:nr_train]
+        Xte = X[nr_train:]
+        Yte = Y[nr_train:]
+    return Xtr, Ytr, Xte, Yte
 
 if __name__ == '__main__':
     read_CIFAR10('cifar-10-batches-py')
